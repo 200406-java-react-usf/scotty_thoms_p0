5b53441ad474b5b02a745d61c6c9a2b7ed58cb38
Scotty Thoms <spthoms19@gmail.com>
2020-04-25 14:09:00 -0500

    WIP index file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import { UserRepository } from './repos/user-repo';
 
 const userRepo = UserRepository.getInstance();
-
-let a = async () => {
-    let output = await userRepo.getAll(); 
-    console.log(output);
-};
