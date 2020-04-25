b9373fc7a078e05681631ed6c0bab45e1b437c45
Scotty Thoms <spthoms19@gmail.com>
2020-04-25 13:04:41 -0500

    added a single error message to lay groundwork for error handling

---
+++ src/errors/errors.ts
@@ -0,0 +1,22 @@
+class ApplicationError {
+
+    message: string;
+    reason: string;
+
+    constructor(rsn?: string) {
+        this.message = 'Unexpected error occured.';
+        rsn ? (this.reason = rsn) : this.reason = 'Unspecified reason';
+    }
+
+    setMessage(message: string) {
+        this.message = message;
+    }
+}
+
+class WorkInProgressError extends ApplicationError {
+
+    constructor(reason?: string) {
+        super(reason);
+        super.setMessage('Not implemented yet. Work in progress.');
+    }
+}
