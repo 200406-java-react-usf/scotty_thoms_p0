543075383fda972c9f03615a036c7877e0faa9a9
Scotty Thoms <spthoms19@gmail.com>
2020-04-25 14:08:35 -0500

    WIP test cases

---
+++ src/__tests__/user-repo.test.ts
@@ -0,0 +1,37 @@
+import { UserRepository as sut } from '../repos/user-repo';
+// import { User } from '../models/user';
+// import  Validator  from '../util/validator';
+// import { WipError, ResourceNotFoundError } from '../errors/errors';
+
+describe( 'user-repo', () => {
+
+    test('should be a singleton', () => {
+        
+        //Arrange
+        expect.assertions(1);
+
+        //Act
+        let ref1 = sut.getInstance();
+        let ref2 = sut.getInstance();
+
+        //Assert
+        expect(ref1).toEqual(ref2);
+    });
+
+    test('should return all users (without passwords) when getAll is called', async () => {
+        
+        //Arrange
+        expect.assertions(3);
+
+        //Act
+        let result = await sut.getInstance().getAll();
+
+        //Assert
+        expect(result).toBeTruthy();
+        expect(result.length).toBeGreaterThan(0);
+        expect(result[0].password).toBeUndefined();
+    })
+
+    test('should ')
+
+});
