428125384c0b8b80601df37902963ff6bb3f9a5b
Scotty Thoms <spthoms19@gmail.com>
2020-04-25 14:36:03 -0500

    changed index file to include express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import express from 'express';
+import * as bodyparser from 'body-parser';
 import { UserRepository } from './repos/user-repo';
+import { UserRouter } from './routers/user-router';
 
-const userRepo = UserRepository.getInstance();
+const app = express();
+
+app.use('/', bodyparser.json());
+
+app.use('/users', UserRouter);
+
+app.listen(8080, () => {
+    console.log(`Application running and listening at: http://localhost:8080`);
+});
