c9cce6e661d1405390d4e2ece058cb9f20008cb4
Scotty Thoms <spthoms19@gmail.com>
2020-04-26 17:13:35 -0500

    added all router files to index.js

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import * as bodyparser from 'body-parser';
 import { UserRepository } from './repos/user-repo';
 import { UserRouter } from './routers/user-router';
+import { TransactionRouter } from './routers/transaction-router';
+import { AccountRouter } from './routers/account-router';
 
 const app = express();
 
 app.use('/', bodyparser.json());
 
 app.use('/users', UserRouter);
+app.use('/transactions', TransactionRouter);
+app.use('/accounts', AccountRouter);
 
 app.listen(8080, () => {
     console.log(`Application running and listening at: http://localhost:8080`);
