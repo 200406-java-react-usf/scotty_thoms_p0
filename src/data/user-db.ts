87d6bf30a4cd62861b980e88c4db0e7a955f640c
Scotty Thoms <spthoms19@gmail.com>
2020-04-26 16:35:45 -0500

    added new user to user-db

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -3,5 +3,6 @@ let id = 1;
 
 export default [
     new User (id++, 'firstUsername', 'password', 'Scotty', 'Thoms', 'email@tester.com', 'Admin'),
-    new User (id++, 'secondUser', 'password', 'Maeve', 'Other', 'maeve@tester.com', 'User')
+    new User (id++, 'secondUser', 'password', 'Maeve', 'Other', 'maeve@tester.com', 'User'),
+    new User (id++, 'jacksonB', 'jackson444', 'Jackson', 'Brown', 'jbrown4@email.com', 'User')
 ];
