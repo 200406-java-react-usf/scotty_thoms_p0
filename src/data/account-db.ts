9cc1f22d8bc631b197bdfdc51eb2e9eb1b357101
Scotty Thoms <spthoms19@gmail.com>
2020-04-26 16:40:26 -0500

    created account database

---
+++ src/data/account-db.ts
@@ -0,0 +1,8 @@
+import { Account } from '../models/account';
+
+export default [
+    new Account (382820, 1000, 'Savings', 2),
+    new Account (228031, 8040, 'Checking', 2), 
+    new Account (302913, 10000, 'Savings', 3),
+    new Account (100001, 40500, 'Checking', 1 )
+]
