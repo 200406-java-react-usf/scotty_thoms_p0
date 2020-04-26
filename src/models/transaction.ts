8918c2992fbdef54a0ca14692314f9a8c8479d03
Scotty Thoms <spthoms19@gmail.com>
2020-04-26 16:32:51 -0500

    transaction model updated

---
+++ src/models/transaction.ts
@@ -0,0 +1,14 @@
+export class transaction {
+
+    id: number;
+    amount: number;
+    description: string;
+    accountId: number;
+
+    constructor(id: number, amt: number, descrip: string, aId: number){
+        this.id = id;
+        this.amount = amt;
+        this.description = descrip;
+        this.accountId = aId;
+    }
+}
