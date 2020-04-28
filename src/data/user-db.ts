e3adbf115b8c3ed79074f27078d9c16d82aea18e
Scotty Thoms <spthoms19@gmail.com>
2020-04-28 15:19:18 -0500

    updated user-db usernames that are easier to remember

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -2,7 +2,7 @@ import { User } from '../models/user';
 let id = 1;
 
 export default [
-    new User (id++, 'firstUsername', 'scotty', 'Scotty', 'Thoms', 'email@tester.com', 'Admin'),
-    new User (id++, 'secondUser', 'maeve', 'Maeve', 'Dubya', 'maeve@tester.com', 'User'),
-    new User (id++, 'jacksonB', 'jackson', 'Jackson', 'Brown', 'jbrown4@email.com', 'User')
+    new User (id++, 'sthoms', 'scotty', 'Scotty', 'Thoms', 'email@tester.com', 'Admin'),
+    new User (id++, 'mdubya', 'maeve', 'Maeve', 'Dubya', 'maeve@tester.com', 'User'),
+    new User (id++, 'jacksonb', 'jackson', 'Jackson', 'Brown', 'jbrown4@email.com', 'User')
 ];
