70b75710123f957cfe186bd8826286d14e97ed42
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 15:48:52 -0500

    updated static array to match db

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -2,7 +2,7 @@ import { User } from '../models/user';
 let id = 1;
 
 export default [
-    new User (id++, 'sthoms', 'scotty', 'Scotty', 'Thoms', 'email@tester.com', 'Admin'),
-    new User (id++, 'mdubya', 'maeve', 'Maeve', 'Dubya', 'maeve@tester.com', 'User'),
-    new User (id++, 'jacksonb', 'jackson', 'Jackson', 'Brown', 'jbrown4@email.com', 'User')
+    new User (id++, 'sthoms', 'scotty', 'Scotty', 'Thoms', 'Admin'),
+    new User (id++, 'mdubya', 'maeve', 'Maeve', 'Dubya', 'User'),
+    new User (id++, 'jacksonb', 'jackson', 'Jackson', 'Brown', 'User')
 ];
