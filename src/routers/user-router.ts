072719ef9186f634fcde77e2faf0dbfeebaaf409
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 16:39:29 -0500

    removed .send() after .json() to remove double sending in user-router

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -21,8 +21,8 @@ UserRouter.get('/:id', async (req, resp) => {
     const id = +req.params.id; //the plus sign is to type coerce id into a number
     try { 
         let payload = await userService.getUserById(id);
-        resp.status(200).json(payload).send();
+        resp.status(200).json(payload);
     } catch (e) {
-        resp.status(404).json(e).send();
+        resp.status(404).json(e);
     }
 })
