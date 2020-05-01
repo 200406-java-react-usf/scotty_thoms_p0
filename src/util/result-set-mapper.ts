9bde1f5371a4931f0cc43df343c2214b53a34aba
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 15:47:20 -0500

    added result-set-mapper to improve readability

---
+++ src/util/result-set-mapper.ts
@@ -0,0 +1,18 @@
+import { User } from "../models/user";
+import { UserSchema } from "./schemas";
+
+export function mapUserResultSet(resultSet: UserSchema): User {
+
+	if (!resultSet) {
+		return {} as User;
+	}
+
+	return new User (
+		resultSet.id,
+		resultSet.username,
+		resultSet.password,
+		resultSet.first_name,
+		resultSet.last_name,
+		resultSet.role_name
+	);
+}
