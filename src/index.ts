80cc2c35506e395aebcc0ebb73a659843b67cbef
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 15:53:03 -0500

    added database config to index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import * as bodyparser from 'body-parser';
-import { UserRepository } from './repos/user-repo';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+
 import { UserRouter } from './routers/user-router';
 import { TransactionRouter } from './routers/transaction-router';
 import { AccountRouter } from './routers/account-router';
+import { Pool } from 'pg';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { corsFilter } from './middleware/cors-filter';
 
+// environment configuration
+dotenv.config();
+
+// database configuration
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
+});
+
+// logging configuration
+fs.mkdir(`${__dirname}/logs`, () => {});
+const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+
+// web server configuration
 const app = express();
-
-app.use('/', bodyparser.json());
-
+app.use(morgan('combined', {stream: logStream}));
+app.use('/', express.json());
+app.use(sessionMiddleware);
+app.use(corsFilter);
 app.use('/users', UserRouter);
 app.use('/transactions', TransactionRouter);
 app.use('/accounts', AccountRouter);
