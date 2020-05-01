8e11214fce8ee3b43b1f2301e183e6e080de24b9
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 15:45:20 -0500

    added a services layer

---
+++ src/services/user-service.ts
@@ -0,0 +1,49 @@
+import { UserRepository } from "../repos/user-repo";
+import { ResourceNotFoundError, BadRequestError, } from "../errors/errors";
+import { isValidId, isEmptyObject } from '../util/validator';
+import { User } from "../models/user";
+
+export class UserService {
+    constructor (private userRepo: UserRepository) {
+        this.userRepo = userRepo;
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        try {
+            let users = await this.userRepo.getAll();
+
+            if (users.length === 0) {
+                throw new ResourceNotFoundError();
+            }
+
+            return users.map(this.removePassword);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getUserById(id: number): Promise<User> {
+        try {
+            if(!isValidId(id)) {
+                throw new BadRequestError();
+            }
+
+            let user = await this.userRepo.getById(id);
+
+            if (isEmptyObject(user)) {
+                throw new ResourceNotFoundError();
+            }
+
+            return this.removePassword(user);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    private removePassword(user: User): User {
+        if(!user || !user.password) return user;
+        let usr = {...user};
+        delete usr.password;
+        return usr;   
+    }
+}
