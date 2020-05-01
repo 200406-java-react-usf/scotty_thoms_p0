7b3a4dad100fb7506f87cee606699c9b6a0a4ca3
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 17:03:07 -0500

    added account service to app.ts instead of singleton

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,15 @@
 import { UserRepository } from "../repos/user-repo";
 import { UserService } from "../services/user-service";
+import { AccountRepository } from "../repos/account-repo";
+import { AccountService } from '../services/account-service';
 
 const userRepo = new UserRepository();
 const userService = new UserService(userRepo);
 
+const accountRepo = new AccountRepository();
+const accountService = new accountService(accountRepo);
+
 export default {
-    userService
+    userService,
+    accountService
 }
