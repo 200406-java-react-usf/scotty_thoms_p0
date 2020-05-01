7be5f46776ec71ba69c9c777923bfaab1549a78c
Scotty Thoms <spthoms19@gmail.com>
2020-05-01 17:03:50 -0500

    changed the account repo to actually talk to the db

--- src/repos/account-repo.ts
+++ src/repos/account-repo.ts
@@ -5,67 +5,84 @@ import Validator from '../util/validator';
 import {
     WipError,
     ResourceNotFoundError,
-    BadRequestError
+    BadRequestError,
+    InternalServerError
 } from '../errors/errors';
+import { PoolClient } from 'pg';
+import { connectionPool } from '..';
+import { mapUserResultSet } from '../util/result-set-mapper';
+import { mapAccountResultSet } from '../util/account-set-mapper';
 
 export class AccountRepository implements CrudRepository<Account> {
     
-    private static instance: AccountRepository;
-
-    private constructor() {};
-
-    static getInstance() {
-        return !AccountRepository.instance ? AccountRepository.instance = new AccountRepository() : AccountRepository.instance;
-    }
+    baseQuery = `
+    select
+        a.id,
+        a.balance,
+        a.type,
+        a.owner_id
+    from accounts a
+    `
     
-    getAll(): Promise<Account[]> {
-        return new Promise((resolve, reject) => {
+    async getAll(): Promise<Account[]> {
+        let client: PoolClient;
+            try { 
+                client = await connectionPool.connect();
+                let sql = `${this.baseQuery} order by a.id`;
+                let rs = await client.query(sql);
+                return rs.rows;
 
-            setTimeout( () => {
-                let allAccounts = [];
-
-                for(let account of data){
-                    allAccounts.push({...account});
-                }
-                if(allAccounts.length === 0){
-                    reject(new ResourceNotFoundError('No accounts found'));
-                }
-    
-                resolve(allAccounts);
-            }, 1000);
-           
-        });
+            } catch (e) {
+                throw new InternalServerError();
+            } finally {
+                client && client.release();
+            }
     };
 
-    getById(id: number): Promise<Account> {
-        return new Promise((resolve, reject) => {
+    async getById(id: number): Promise<Account> {
+        let client: PoolClient;
+            try { 
+                client = await connectionPool.connect();
+                let sql = `${this.baseQuery} where a.id = $1`;
+                let rs = await client.query(sql, [id]);
+                return mapAccountResultSet(rs.rows[0]);
 
-            if (!Validator.isValidId(id)) {
-                reject(new BadRequestError());
+            } catch (e) {
+                throw new InternalServerError();
+            } finally {
+                client && client.release();
             }
-
-            setTimeout(() => {
-                const act = {...data.find(act => act.id === id)};
-
-                if (Object.keys(act).length === 0){
-                    reject(new ResourceNotFoundError());
-                    return;
-                }
-
-                resolve(act);
-            }, 1000);
-        });
     }
 
-    save(newTransaction: Account): Promise<Account> {
-        return new Promise((resolve, reject) => {
-            reject(new WipError());
-        });
+    async save(newTransaction: Account): Promise<Account> {
+        //WIP!
+        let client: PoolClient;
+            try { 
+                client = await connectionPool.connect();
+                let sql = `${this.baseQuery}`;
+                let rs = await client.query(sql);
+                return mapAccountResultSet(rs.rows[0]);
+
+            } catch (e) {
+                throw new InternalServerError();
+            } finally {
+                client && client.release();
+            }
     }
 
-    update(updatedTransaction: Account): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            reject(new WipError());
-        });
+    async update(updatedTransaction: Account): Promise<boolean> {
+        //WIP!
+        let client: PoolClient;
+            try { 
+                client = await connectionPool.connect();
+                let sql = `${this.baseQuery}`;
+                let rs = await client.query(sql);
+                return true;
+
+            } catch (e) {
+                throw new InternalServerError();
+            } finally {
+                client && client.release();
+            }
     }
 }
