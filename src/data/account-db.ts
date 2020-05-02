4742402f884d45ac1865ccf470e5aea1896d3d0b
Scotty Thoms <spthoms19@gmail.com>
2020-05-02 11:07:26 -0500

    removed all static arrays so application is 100% using database

--- src/data/account-db.ts
+++
@@ -1,8 +0,0 @@
-import { Account } from '../models/account';
-
-export default [
-    new Account (382820, 1000, 'Savings', 2),
-    new Account (228031, 8040, 'Checking', 2), 
-    new Account (302913, 10000, 'Savings', 3),
-    new Account (100001, 40500, 'Checking', 1 )
-]
--- src/data/user-db.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../models/user';
-let id = 1;
-
-export default [
-    new User (id++, 'sthoms', 'scotty', 'Scotty', 'Thoms', 'Admin'),
-    new User (id++, 'mdubya', 'maeve', 'Maeve', 'Dubya', 'User'),
-    new User (id++, 'jacksonb', 'jackson', 'Jackson', 'Brown', 'User')
-];
