975e2b158c4dc46388694aa73e7b410131299d5c
Scotty Thoms <spthoms19@gmail.com>
2020-05-02 09:24:42 -0500

    fixed double send

--- src/routers/account-router.ts
+++ src/routers/account-router.ts
@@ -9,19 +9,19 @@ const accountService = AppConfig.accountService;
 
 AccountRouter.get('', async (req, resp) => {
     try{
-        let payload = await accountService.getAll();
-        resp.status(200).json(payload).send();
+        let payload = await accountService.getAllAccounts();
+        resp.status(200).json(payload);
     } catch (e){
-        resp.status(404).json(e).send();
+        resp.status(404).json(e);
     }
 });
 
 AccountRouter.get('/:id', async (req, resp) => {
     const id = +req.params.id; //the plus sign is to type coerce id into a number
     try { 
-        let payload = await accountService.getById(id);
-        resp.status(200).json(payload).send();
+        let payload = await accountService.getAccountById(id);
+        resp.status(200).json(payload);
     } catch (e) {
-        resp.status(404).json(e).send();
+        resp.status(404).json(e);
     }
 })
