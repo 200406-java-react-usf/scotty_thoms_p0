14f1f81781043ec4aec39ad903eeb7ccb0e79c0e
Scotty Thoms <spthoms19@gmail.com>
2020-05-02 10:39:48 -0500

    added service layer for transactions and connected to repo layer

---
+++ src/services/transaction-service.ts
@@ -0,0 +1,42 @@
+import { TransactionRepository } from "../repos/transaction-repo";
+import { ResourceNotFoundError, BadRequestError } from "../errors/errors";
+import { Transaction } from "../models/transaction";
+import { isValidId, isEmptyObject } from "../util/validator";
+
+export class TransactionService {
+    constructor (private transactionRepo: TransactionRepository) {
+        this.transactionRepo = transactionRepo;
+    }
+
+    async getAllTransaction(): Promise<Transaction[]> {
+        try {
+            let transactions = await this.transactionRepo.getAll();
+
+            if (transactions.length === 0) {
+                throw new ResourceNotFoundError();
+            }
+
+            return transactions;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getTransactionById(id: number): Promise<Transaction> {
+        try {
+            if (!isValidId(id)) {
+                throw new BadRequestError();
+            }
+
+            let transaction = await this.transactionRepo.getById(id);
+
+            if (isEmptyObject(transaction)) {
+                throw new ResourceNotFoundError();
+            }
+
+            return transaction;
+        } catch (e) {
+            throw e;
+        }
+    }
+}
