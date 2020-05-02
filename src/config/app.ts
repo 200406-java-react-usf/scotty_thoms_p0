7a6bb2d6194634bf34c05637a5abee9e6cd749f4
Scotty Thoms <spthoms19@gmail.com>
2020-05-02 10:42:48 -0500

    added config for transaction service

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,8 @@ import { UserRepository } from "../repos/user-repo";
 import { UserService } from "../services/user-service";
 import { AccountRepository } from "../repos/account-repo";
 import { AccountService } from '../services/account-service';
+import { TransactionRepository } from "../repos/transaction-repo";
+import { TransactionService } from "../services/transaction-service";
 
 
 const userRepo = new UserRepository();
@@ -10,7 +12,11 @@ const userService = new UserService(userRepo);
 const accountRepo = new AccountRepository();
 const accountService = new AccountService(accountRepo);
 
+const transactionRepo = new TransactionRepository();
+const transactionService = new TransactionService(transactionRepo);
+
 export default {
     userService,
-    accountService
+    accountService,
+    transactionService
 }
