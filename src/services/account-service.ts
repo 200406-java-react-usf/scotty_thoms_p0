c0315ddf9df63b36c7e69c50cf3e5dbcdde5d355
Scotty Thoms <spthoms19@gmail.com>
2020-05-03 23:31:53 -0500

    added functionality for checking to make sure user exists before creating associated account - also added getAccountByUniqueKey for extra layer of security

--- src/services/account-service.ts
+++ src/services/account-service.ts
@@ -1,7 +1,7 @@
 import { Account } from '../models/account'
 import { AccountRepository } from "../repos/account-repo";
-import { ResourceNotFoundError, BadRequestError } from "../errors/errors";
-import { isValidId, isEmptyObject, isValidObject } from '../util/validator';
+import { ResourceNotFoundError, BadRequestError, ResourcePersistenceError } from "../errors/errors";
+import { isValidId, isEmptyObject, isValidObject, isPropertyOf, isValidStrings } from '../util/validator';
 
 export class AccountService {
     constructor (private accountRepo: AccountRepository) {
@@ -42,14 +42,65 @@ export class AccountService {
             throw new BadRequestError();
         }
 
-        // let userExists = await this.accountRepo.checkOwnerId(newAccount.id);
+        let ownerExists = await this.checkOwnerExists(newAccount.ownerId);
 
-        // if (!userExists) {
-        //     throw new BadRequestError('You cannot make an account for a user that does not exist');
-        // }
+        if (!ownerExists) {
+            throw new ResourcePersistenceError('No account exists with User provided.')
+        }
         
         const persistedAccount = await this.accountRepo.save(newAccount);
 
         return persistedAccount;
+    }
+
+    async getAccountByUniqueKey(queryObj: any): Promise<Account> {
+
+        try {
+
+            let queryKeys = Object.keys(queryObj);
+
+            if(!queryKeys.every(key => isPropertyOf(key, Account))) {
+                throw new BadRequestError();
+            }
+
+            // only supports single param searches (for now)
+            let key = queryKeys[0];
+            let val = queryObj[key];
+
+            // if they are searching for an account by id, reuse the logic we already have
+            if (key === 'id') {
+                return await this.getAccountById(+val);
+            }
+
+            if(!isValidStrings(val)) {
+                throw new BadRequestError();
+            }
+
+            // have to change wording to work with db
+            if (key === 'ownerId') {
+                key = 'owner_id';
+            }
+
+            let account = await this.accountRepo.getAccountByUniqueKey(key, val);
+            if (isEmptyObject(account)) {
+                throw new ResourceNotFoundError();
+            }
+            return account;
+
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async checkOwnerExists(ownerId: number): Promise<boolean> {
+        try {
+            await this.getAccountByUniqueKey({'ownerId': ownerId})
+        } catch (e) {
+            console.log(`No account found with id ${ownerId}. Try again.`);
+            return false;
+        }
+
+        console.log(`Account exists with id ${ownerId}. Proceed.`);
+        return true;
     }
 }
