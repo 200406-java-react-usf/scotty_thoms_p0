cdd50996b51c9679606f6aba8e3b2d0b0b88b24e
Scotty Thoms <spthoms19@gmail.com>
2020-05-05 23:16:09 -0500

    added more testing for account repo

--- src/__tests__/account-repo.test.ts
+++ src/__tests__/account-repo.test.ts
@@ -61,6 +61,85 @@ describe('accountRepo', () => {
         expect(mockConnect).toBeCalledTimes(1);
     });
 
+    test('should throw an error when getAll is called with no data in db', async () => {
+
+        //Arrange
+        expect.hasAssertions();
+        (mockConnect as jest.Mock).mockImplementation( () => {
+            return {
+                query: jest.fn().mockImplementation( () => {
+                    return {rows: []};
+                }),
+                release: jest.fn()
+            };
+        });
+
+        // Act
+        let result = await sut.getAll();
+
+        // Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof Array).toBe(true);
+        expect(result.length).toBe(0);
+        expect(mockConnect).toBeCalledTimes(1);
+    });
+
+    test('should return the updated account when update is given a valid account to update', async() => {
+
+        expect.hasAssertions();
+
+        let mockAccount = new Account(1, 400, 'test', 1);
+        (mockConnect as jest.Mock).mockImplementation(() => {
+            return {
+                query: jest.fn().mockImplementation(() => {
+                    return mockAccount;
+                }),
+                release: jest.fn()
+            };
+        });
+
+        let result = await sut.update(mockAccount);
+
+        expect(result).toBeTruthy();
+
+    });
+
+    test('should resolve to an Account object when checkOwnerExists is called with valid account id', async () => {
+        
+        //Arrange
+        expect.hasAssertions();
+
+        let mockAccount = new Account(1, 400, 'test', 1);
+        (mockMapper.mapAccountResultSet as jest.Mock).mockReturnValue(mockAccount);
+
+        //Act
+        let result = await sut.checkOwnerExists(1);
+
+        //Assert
+        expect(result).toBeTruthy();
+        expect(result instanceof Account).toBe(true);
+        expect(mockConnect).toBeCalledTimes(1);
+    });
+    
+    test('should return the updated account when delete is given a valid account to delete', async() => {
+
+        expect.hasAssertions();
+
+        let mockAccount = new Account(1, 400, 'test', 1);
+        (mockConnect as jest.Mock).mockImplementation(() => {
+            return {
+                query: jest.fn().mockImplementation(() => {
+                    return true;
+                }),
+                release: jest.fn()
+            };
+        });
+
+        let result = await sut.delete(mockAccount);
+
+        expect(result).toBeTruthy();
+
+    });
     test('should resolve to an empty array when getAll retrieves no records from data source', async () => {
 
         //Arrange
